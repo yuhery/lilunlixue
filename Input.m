6fe827d79ae1df9736036ca87fdc0a2637fd3b72
yuhery <yuhery@qq.com>
2016-11-17 22:17:03 +0800

    change the input,make that people could input the points being forced in other places

--- Input.m
+++ Input.m
@@ -1,45 +1,37 @@
 %用户输入
 %输出位置矩阵X，拓扑矩阵Y，外力矩阵Z，节点数W
-function [X,Y,Z,W]=Input()
+function [X,Y,Z,W,M]=Input()
 
-%输入节点数和杆数
-n=input('节点数');
-b=input('杆数');
-%节点位置矩阵?
-position=ones(2,n);
-% 节点拓扑矩阵
-topology=ones(2,b);
-% 外力矩阵
-force=zeros(2*n,1);
+  %输入节点数和杆数
+  n=input('节点数');
+  b=input('杆数');
 
-k=input('1：一次矩阵输入  or 2：分节点输入');
-if(k==1)
-    position=(input('节点位置矩阵'))';
-else
-    for i=1:1:n
-    position(:,i)=input('节点位置');
-    end
-end
+  if ((2*n-3)==b)
+  % 外力矩阵
+  force=zeros(2*n,1);
 
-%输入拓扑矩阵
-m=input('1：一次矩阵输入  or 2：分杆输入');
-if(m==1)
-    topology=(input('拓扑关系矩阵'))';
-else
-    for i=1:1:b
-	topology(:,i)=input('杆两端节点序号');
-    end
-end
+  %输入位置矩阵
+  position=(input('节点位置矩阵'))';
 
-forceNumber=input('有几个节点受外力');
-for i=1:1:forceNumber
+  %输入约束点序号
+  order=input('被约束的两个节点的位置顺序是');
+
+  %输入拓扑矩阵
+  topology=(input('拓扑关系矩阵'))';
+
+  forceNumber=input('有几个节点受外力');
+  for i=1:1:forceNumber
     number1=input('第几号节点');
     force1=input('外力大小');
     force(2*number1-1,1)=force1(1,1);
     force(2*number1,1)=force1(1,2);
-end
+  end
 
-X=position;
-Y=topology;
-Z=force;
-W=n;
+  X=position;
+  Y=topology;
+  Z=force;
+  W=n;
+  M=order;
+else
+    disp('你输入的不是合法值');
+  end
--- Mainh.m
+++ Mainh.m
@@ -1,10 +1,10 @@
 clear;
 %输入
-[position,topology,force,n]=Input();
+[position,topology,force,n,order]=Input();
 
 %求解engel和H
 angel=forAngel(position,topology,n);
-H=forH(position,topology,angel,n);
+H=forH(position,topology,angel,n,order);
 
 %求解x
 x=-inv(H)*force;
